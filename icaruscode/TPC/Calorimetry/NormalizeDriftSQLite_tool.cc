2558d52760b51843233d76b462b14efed699be09
Bruce Howard <howard@fnal.gov>
2022-11-06 23:35:08 -0600

    hack lifetime correction to use a value for Run 7033

--- icaruscode/TPC/Calorimetry/NormalizeDriftSQLite_tool.cc
+++ icaruscode/TPC/Calorimetry/NormalizeDriftSQLite_tool.cc
@@ -119,6 +119,9 @@ double icarus::calo::NormalizeDriftSQLite::Normalize(double dQdx, const art::Eve
   // WW
   if (cryo == 1 && (tpc == 2 || tpc == 3)) thiselifetime = runelifetime.tau_WW;
   
+  // BH HACK FOR NUE CANDIDATE EVENT
+  thiselifetime = 3333.33; // ns
+
   // Get the hit time
   double thit = clock_data.TPCTick2TrigTime(hit.PeakTime()) - t0;
 
